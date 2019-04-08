2ee859eef0245965e6ce2184a654cb48ffe49105
Deddy Kakunsi <dkakunsi@dgitsystems.com>
2019-04-08 15:33:11 +0800

    Add webapi that use net-logging.

---
+++ asp-webapi/Middleware/Message.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace asp_webapi.Middleware {
+  public class Message {
+    
+    private RequestDelegate _next;
+
+    public Message(RequestDelegate next) {
+      _next = next;
+    }
+
+    public async Task Invoke(HttpContext context) {
+      String correlationId = context.Request.Headers["correlationId"];
+      log4net.ThreadContext.Properties["correlationId"] = correlationId;
+      log4net.GlobalContext.Properties["service"] = "BusinessInteraction";
+
+      await _next.Invoke(context);
+    }
+  }
+}
