82c47a2e5795356eb38ccb288cc59c66dc1fe638
Deddy Kakunsi <dkakunsi@dgitsystems.com>
2019-04-10 20:01:15 +0800

    Add component to just reuse log4net pattern.

---
+++ net-logging/Converter/CustomStackTraceConverter.cs
@@ -0,0 +1,79 @@
+using log4net.Core;
+using log4net.Layout.Pattern;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace net_logging.Converter
+{
+    public class CustomStackTraceConverter : PatternLayoutConverter
+    {
+        public bool IsRecursive { get; set; }
+
+        protected override void Convert(TextWriter writer, LoggingEvent loggingEvent)
+        {
+            var format = JsonConvert.SerializeObject (GenerateStackTrace (loggingEvent.ExceptionObject, IsRecursive));
+            writer.Write (format);
+        }
+
+        private List<Object> GenerateStackTrace (Exception ex, bool recursive)
+        {
+            if (ex == null) {
+                return null;
+            }
+
+            var stack = new List<Object> ();
+
+            if (recursive)
+            {
+                // All exception stack will be printed.
+
+                if (ex.InnerException != null)
+                {
+                    stack.AddRange (GenerateStackTrace (ex.InnerException, recursive));
+                }
+            }
+            else
+            {
+                ex = ExtractInnerException(ex);
+            }
+
+            stack.Add (new 
+            {
+                exception = ex.GetType ().FullName,
+                message = ex.Message,
+                stack = GenerateStackTrace (new StackTrace (ex, true))
+            });
+
+            return stack;
+        }
+
+        private Exception ExtractInnerException(Exception ex) {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            return ex;
+        }
+
+        private List<Object> GenerateStackTrace (StackTrace trace)
+        {
+            var stack = new List<Object> ();
+
+            foreach (StackFrame frame in trace.GetFrames ())
+            {
+                stack.Add (new 
+                {
+                    file = frame.GetFileName (),
+                    method = frame.GetMethod ().Name,
+                    line = frame.GetFileLineNumber ()
+                });
+            }
+
+            return stack;
+        }
+    }
+}
